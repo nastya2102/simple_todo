ee544dd0e41a92794b1406ef8182122996a39226
Anastasia Kuznetsova <nastya2102@icloud.com>
2019-09-06 16:30:01 +0300

    Updates

---
+++ precache-manifest.753ac083381efc3bad3102161222e66f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b286a75b7c402a2f15e9cea544be2d",
+    "url": "/nastya2102/simple_todo/index.html"
+  },
+  {
+    "revision": "20d88cf8ef64a0ee5924",
+    "url": "/nastya2102/simple_todo/static/css/main.e790613d.chunk.css"
+  },
+  {
+    "revision": "a80d45e1fe5e6331f38d",
+    "url": "/nastya2102/simple_todo/static/js/2.285d6ce5.chunk.js"
+  },
+  {
+    "revision": "20d88cf8ef64a0ee5924",
+    "url": "/nastya2102/simple_todo/static/js/main.70b5dd75.chunk.js"
+  },
+  {
+    "revision": "e3afa9d9c24a3303ef69",
+    "url": "/nastya2102/simple_todo/static/js/runtime~main.a73edb5f.js"
+  }
+]);
